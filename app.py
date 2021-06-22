3184301b8f473c7ee7a34e851809afd1b5db4eda
Hariharan2907 <56897205+Hariharan2907@users.noreply.github.com>
2021-06-22 12:46:59 -0500

    Updated formatting of tables
    Removed index, changed formatting of numbers, and separated into tele/non tele tables

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ url = "cost.csv"
 sda = []
 
 df = pd.read_csv(url,error_bad_lines=False)
+df = df.rename(columns={'rgrp3':'Child', 'rgrp4': 'Blind/Disabled','rgrp6':'Telemonitoring','sfy':'Year','SDA' : 'Region','tot_tvst':'Televisits'})
 df = df.dropna()
-df = df.rename(columns={'rgrp3':'Child', 'rgrp4': 'Blind/Disabled','rgrp6':'Telemonitoring','sfy':'Year'})
+df = df[["Region","Year","Televisits","Blind/Disabled","Child","Telemonitoring","treat","meas","type","post"]]
 
-sda = (df['SDA'].unique())
+sda = (df['Region'].unique())
 
 
 
@@ -29,34 +30,44 @@ med_df_precost = med_df[med_df['post']==0]
 #post cost
 med_df_postcost = med_df[med_df['post']==1]
 
-texas_nclient = nclients[nclients['SDA']=='Texas']
-texas_nclient=texas_nclient.drop(['type','post','meas','tot_tvst'],axis=1)
-texas_nclient['Blind/Disabled'] = texas_nclient['Blind/Disabled'].astype('int64')
-texas_nclient['Child'] = texas_nclient['Child'].astype('int64')
-texas_nclient['Telemonitoring'] = texas_nclient['Telemonitoring'].astype('int64')
+texas_nclient = nclients[nclients['Region']=='Texas']
+texas_nclient=texas_nclient.drop(['type','post','meas'],axis=1)
 texas_nclient = texas_nclient.sort_values(by=['treat'])
+texas_nclient['Child'] = texas_nclient['Child'].map('{:,.0f}'.format)
+texas_nclient['Blind/Disabled'] = texas_nclient['Blind/Disabled'].map('{:,.0f}'.format)
+texas_nclient['Telemonitoring'] = texas_nclient['Telemonitoring'].map('{:,.0f}'.format)
+texas_nclient['Televisits'] = texas_nclient['Televisits'].map('{:,.0f}'.format)
+texas_nclient_tele = texas_nclient[texas_nclient['treat']==1]
+texas_nclient_nontele = texas_nclient[texas_nclient['treat']==0]
 
-texas_precost = med_df_precost[med_df_precost['SDA']=='Texas']
-texas_precost=texas_precost.drop(['type','post','meas','tot_tvst'],axis=1)
-texas_precost['Blind/Disabled'] = texas_precost['Blind/Disabled'].apply(np.ceil)
-texas_precost['Blind/Disabled'] = texas_precost['Blind/Disabled'].astype('int64')
-texas_precost['Child'] = texas_precost['Child'].apply(np.ceil)
-texas_precost['Child'] = texas_precost['Child'].astype('int64')
-texas_precost['Telemonitoring'] = texas_precost['Telemonitoring'].apply(np.ceil)
-texas_precost['Telemonitoring'] = texas_precost['Telemonitoring'].astype('int64')
+texas_precost = med_df_precost[med_df_precost['Region']=='Texas']
+texas_precost=texas_precost.drop(['type','post','meas'],axis=1)
+texas_precost['Blind/Disabled'] = texas_precost['Blind/Disabled'].apply(np.round)
+texas_precost['Child'] = texas_precost['Child'].apply(np.round)
+texas_precost['Telemonitoring'] = texas_precost['Telemonitoring'].apply(np.round)
 texas_precost = texas_precost.sort_values(by=['treat'])
+texas_precost['Child'] = texas_precost['Child'].map('${:,.0f}'.format)
+texas_precost['Blind/Disabled'] = texas_precost['Blind/Disabled'].map('${:,.0f}'.format)
+texas_precost['Telemonitoring'] = texas_precost['Telemonitoring'].map('${:,.0f}'.format)
+texas_precost['Televisits'] = texas_precost['Televisits'].map('${:,.0f}'.format)
+texas_precost_tele = texas_precost[texas_precost['treat']==1]
+texas_precost_nontele = texas_precost[texas_precost['treat']==0]
 
-texas_postcost = med_df_postcost[med_df_postcost['SDA']=='Texas']
-texas_postcost=texas_postcost.drop(['type','post','meas','tot_tvst'],axis=1)
-texas_postcost['Blind/Disabled'] = texas_postcost['Blind/Disabled'].apply(np.ceil)
-texas_postcost['Blind/Disabled'] = texas_postcost['Blind/Disabled'].astype('int64')
-texas_postcost['Child'] = texas_postcost['Child'].apply(np.ceil)
-texas_postcost['Child'] = texas_postcost['Child'].astype('int64')
-texas_postcost['Telemonitoring'] = texas_postcost['Telemonitoring'].apply(np.ceil)
-texas_postcost['Telemonitoring'] = texas_postcost['Telemonitoring'].astype('int64')
+
+
+
+texas_postcost = med_df_postcost[med_df_postcost['Region']=='Texas']
+texas_postcost=texas_postcost.drop(['type','post','meas'],axis=1)
+texas_postcost['Blind/Disabled'] = texas_postcost['Blind/Disabled'].apply(np.round)
+texas_postcost['Child'] = texas_postcost['Child'].apply(np.round)
+texas_postcost['Telemonitoring'] = texas_postcost['Telemonitoring'].apply(np.round)
 texas_postcost = texas_postcost.sort_values(by=['treat'])
-
-
+texas_postcost['Child'] = texas_postcost['Child'].map('${:,.0f}'.format)
+texas_postcost['Blind/Disabled'] = texas_postcost['Blind/Disabled'].map('${:,.0f}'.format)
+texas_postcost['Telemonitoring'] = texas_postcost['Telemonitoring'].map('${:,.0f}'.format) 
+texas_postcost['Televisits'] = texas_postcost['Televisits'].map('${:,.0f}'.format)
+texas_postcost_tele = texas_postcost[texas_postcost['treat']==1]
+texas_postcost_nontele = texas_postcost[texas_postcost['treat']==0]
 
 @app.route('/', methods=["GET","POST"])
 @app.route('/home', methods=["GET","POST"])
@@ -77,38 +88,48 @@ def index():
 def medcost():
     if request.method == "POST":
         sda_name = request.form.get("sda", None)
-        num_client = nclients[nclients['SDA']==sda_name]
+        num_client = nclients[nclients['Region']==sda_name]
         num_client = num_client.sort_values(by=['treat'])
-        num_client=num_client.drop(['type','post','meas','tot_tvst'],axis=1)
-        num_client['Blind/Disabled'] = num_client['Blind/Disabled'].astype('int64')
-        num_client['Child'] = num_client['Child'].astype('int64')
-        num_client['Telemonitoring'] = num_client['Telemonitoring'].astype('int64')
+        num_client=num_client.drop(['type','post','meas'],axis=1)
+        num_client['Child'] = num_client['Child'].map('{:,.0f}'.format)
+        num_client['Blind/Disabled'] = num_client['Blind/Disabled'].map('{:,.0f}'.format)
+        num_client['Telemonitoring'] = num_client['Telemonitoring'].map('{:,.0f}'.format)
+        num_client['Televisits'] = num_client['Televisits'].map('{:,.0f}'.format)
+        
+        style1=num_client.style.hide_index()
 
-        df_precost = med_df_precost[med_df_precost['SDA']==sda_name]
+        html = style1.render()
+        with open('templates/style.html', 'w') as f:
+            f.write(html)
+
+
+        df_precost = med_df_precost[med_df_precost['Region']==sda_name]
         df_precost = df_precost.sort_values(by=['treat'])
-        df_precost=df_precost.drop(['type','post','meas','tot_tvst'],axis=1)
-        df_precost['Blind/Disabled'] = df_precost['Blind/Disabled'].apply(np.ceil)
-        df_precost['Blind/Disabled'] = df_precost['Blind/Disabled'].astype('int64')
-        df_precost['Child'] = df_precost['Child'].apply(np.ceil)
-        df_precost['Child'] = df_precost['Child'].astype('int64')
-        df_precost['Telemonitoring'] = df_precost['Telemonitoring'].apply(np.ceil)
-        df_precost['Telemonitoring'] = df_precost['Telemonitoring'].astype('int64')
+        df_precost=df_precost.drop(['type','post','meas'],axis=1)
+        df_precost['Blind/Disabled'] = df_precost['Blind/Disabled'].apply(np.round)
+        df_precost['Child'] = df_precost['Child'].apply(np.round)
+        df_precost['Telemonitoring'] = df_precost['Telemonitoring'].apply(np.round)
+        df_precost['Child'] = df_precost['Child'].map('${:,.0f}'.format)
+        df_precost['Blind/Disabled'] = df_precost['Blind/Disabled'].map('${:,.0f}'.format)
+        df_precost['Telemonitoring'] = df_precost['Telemonitoring'].map('${:,.0f}'.format)
+        df_precost['Televisits'] = df_precost['Televisits'].map('${:,.0f}'.format)
 
-
-        df_postcost = med_df_postcost[med_df_postcost['SDA']==sda_name]
+        df_postcost = med_df_postcost[med_df_postcost['Region']==sda_name]
         df_postcost = df_postcost.sort_values(by=['treat'])
-        df_postcost=df_postcost.drop(['type','post','meas','tot_tvst'],axis=1)
-        df_postcost['Blind/Disabled'] = df_postcost['Blind/Disabled'].apply(np.ceil)
-        df_postcost['Blind/Disabled'] = df_postcost['Blind/Disabled'].astype('int64')
-        df_postcost['Child'] = df_postcost['Child'].apply(np.ceil)
-        df_postcost['Child'] = df_postcost['Child'].astype('int64')
-        df_postcost['Telemonitoring'] = df_postcost['Telemonitoring'].apply(np.ceil)
-        df_postcost['Telemonitoring'] = df_postcost['Telemonitoring'].astype('int64')
+        df_postcost=df_postcost.drop(['type','post','meas'],axis=1)
+        df_postcost['Blind/Disabled'] = df_postcost['Blind/Disabled'].apply(np.round)
+        df_postcost['Child'] = df_postcost['Child'].apply(np.round)  
+        df_postcost['Telemonitoring'] = df_postcost['Telemonitoring'].apply(np.round)   
+        df_postcost['Child'] = df_postcost['Child'].map('${:,.0f}'.format)
+        df_postcost['Blind/Disabled'] = df_postcost['Blind/Disabled'].map('${:,.0f}'.format)
+        df_postcost['Telemonitoring'] = df_postcost['Telemonitoring'].map('${:,.0f}'.format)     
+        df_postcost['Televisits'] = df_postcost['Televisits'].map('${:,.0f}'.format)
 
         if sda_name != None:
-            return render_template("medcost.html", sda_name=sda_name, sda=sda, num_client=[num_client.to_html(classes='data')], df_precost=[df_precost.to_html(classes='data')], df_postcost=[df_postcost.to_html(classes='data')],  header = "true")
-    return render_template('medcost1.html',sda=sda, texas_nclient=[texas_nclient.to_html(classes='table table-striped')], texas_precost=[texas_precost.to_html(classes='data')], texas_postcost=[texas_postcost.to_html(classes='data')])
-    #return render_template('medcost.html', med_df_precost = [med_df_precost.to_html(classes='data')], header="true", med_df_postcost = [med_df_postcost.to_html(classes='data')])
+            return render_template("medcost.html", sda_name=sda_name, sda=sda, num_client=[num_client.to_html(index = False)], df_precost=[df_precost.to_html(index = False)], df_postcost=[df_postcost.to_html(index = False)])
+    return render_template('medcost1.html',sda=sda, texas_nclient_tele=[texas_nclient_tele.to_html(index=False)], texas_nclient_nontele=[texas_nclient_nontele.to_html(index=False)], texas_precost_tele=[texas_precost_tele.to_html(index=False)], 
+                            texas_precost_nontele=[texas_precost_nontele.to_html(index=False)], texas_postcost_tele=[texas_postcost_tele.to_html(index = False)],  texas_postcost_nontele=[texas_postcost_nontele.to_html(index = False)])
+    
     
 if __name__ == "__main__":
     app.run(debug=True) 
