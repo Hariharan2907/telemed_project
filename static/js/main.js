fab427aa7e968fe2af84c261d090296f48dc1de7
Hariharan2907 <56897205+Hariharan2907@users.noreply.github.com>
2021-07-09 09:58:09 -0500

    Create main.js

---
+++ static/js/main.js
@@ -0,0 +1,45 @@
+function addColumn(tblId)
+{
+	var tblHeadObj = document.getElementById(tblId).tHead;
+	for (var h=0; h<tblHeadObj.rows.length; h++) {
+		var newTH = document.createElement('th');
+		tblHeadObj.rows[h].appendChild(newTH);
+		newTH.innerHTML = 'Cost '  + (tblHeadObj.rows[h].cells.length - 2)
+	}
+
+	var tblBodyObj = document.getElementById(tblId).tBodies[0];
+	for (var i=0; i<tblBodyObj.rows.length; i++) {
+		var newCell = tblBodyObj.rows[i].insertCell(-1);
+		element = document.createElement("input");
+        element.type = "number";
+        element.name = "inter_costs" ;
+        newCell.appendChild(element);
+	}
+}
+
+function deleteColumn(tblId)
+{
+	var allRows = document.getElementById(tblId).rows;
+    
+	for (var i=0; i<allRows.length; i++) {
+		if (allRows[i].cells.length > 1) {
+			allRows[i].deleteCell(-1);
+		}
+	}
+}
+function addRowToTable()
+{
+    var root = document.getElementById('input-table').getElementsByTagName('tbody')[0];
+    var rows = root.getElementsByTagName('tr');
+    var clone = cloneEl(rows[rows.length - 1]);
+    root.appendChild(clone);
+
+}
+function cloneEl(el) {
+    var clo = el.cloneNode(true);
+    return clo;
+}
+function removeRow(oButton) {
+    var tbl = document.getElementById('input-table');
+    tbl.deleteRow(oButton.parentNode.parentNode.rowIndex);       // BUTTON -> TD -> TR.
+}
