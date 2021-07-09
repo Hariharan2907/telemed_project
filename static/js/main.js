137c29bd8d56dc369481a842d8574b26acce972b
Hariharan2907 <56897205+Hariharan2907@users.noreply.github.com>
2021-07-09 17:46:45 -0500

    added functions to add/delete rows

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,7 @@ function addColumn(tblId)
 		element = document.createElement("input");
         element.type = "number";
         element.name = "inter_costs" ;
+        element.step = "any";
         newCell.appendChild(element);
 	}
 }
@@ -31,13 +32,46 @@ function addRowToTable()
 {
     var root = document.getElementById('input-table').getElementsByTagName('tbody')[0];
     var rows = root.getElementsByTagName('tr');
+    
     var clone = cloneEl(rows[rows.length - 1]);
+    
     root.appendChild(clone);
 
 }
 function cloneEl(el) {
     var clo = el.cloneNode(true);
+    
     return clo;
+}
+
+function insertRow(tableID)
+{
+    var x = document.getElementById(tableID);
+    var new_row = x.rows[1].cloneNode(true);
+    var len = x.rows.length;
+    new_row.cells[0].innerHTML = len;
+  
+    var inp1 = new_row.cells[1].getElementsByTagName('input')[0];
+    inp1.id += len;
+    inp1.value = '';
+    var inp2 = new_row.cells[2].getElementsByTagName('input')[0];
+    inp2.id += len;
+    inp2.value = '';
+    x.appendChild(new_row);
+}
+
+function addRows(){ 
+	var table = document.getElementById('input-table');
+	var rowCount = table.rows.length;
+	var cellCount = table.rows[0].cells.length; 
+	var row = table.insertRow(rowCount);
+	for(var i =0; i <= cellCount; i++){
+		var cell = 'cell'+i;
+		cell = row.insertCell(i);
+		var copycel = document.getElementById('col'+i).innerHTML;
+		cell.innerHTML=copycel;
+
+	}
 }
 function removeRow(oButton) {
     var tbl = document.getElementById('input-table');
